2839f8f5d4a243807263db66d2a51eea9bac769f
diegochine <chine.diego@gmail.com>
2021-09-16 14:25:11 +0200

    minor mod

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 from flatland.envs.observations import TreeObsForRailEnv
-from flatland.envs.rail_env import RailEnv
+from flatland.envs.predictions import ShortestPathPredictorForRailEnv
 from flatland.envs.rail_generators import sparse_rail_generator
 from flatland.utils.rendertools import RenderTool
 
 from flatland_agent import FlatlandDQNAgent
 from processor import normalize_observation
+from rail_env import RailEnvWrapper
 
 
-def flatland_test(path, width, height, n_agents, tree_depth, n_episodes=200, steps_to_train=4,
-                  batch_size=128, max_steps=200, output_dir="./output/"):
+def flatland_test(path, width, height, n_agents, tree_depth, n_episodes=10, max_steps=200):
     rail_generator = sparse_rail_generator(max_num_cities=n_agents,
                                            max_rails_between_cities=10, max_rails_in_city=2,
                                            seed=42)
-    env = RailEnv(width=width, height=height,
-                  rail_generator=rail_generator,
-                  number_of_agents=n_agents,
-                  obs_builder_object=TreeObsForRailEnv(max_depth=tree_depth),
-                  random_seed=42)
+    tree_obs = TreeObsForRailEnv(max_depth=tree_depth, predictor=ShortestPathPredictorForRailEnv())
+    env = RailEnvWrapper(width=width, height=height,
+                         rail_generator=rail_generator,
+                         number_of_agents=n_agents,
+                         obs_builder_object=tree_obs,
+                         random_seed=42)
     env_renderer = RenderTool(env)
 
     player = FlatlandDQNAgent.load(path, 'final', training=False)
@@ -40,7 +40,7 @@ def flatland_test(path, width, height, n_agents, tree_depth, n_episodes=200, ste
         step = 0
         done = {'__all__': False}
 
-        while not done['__all__'] and step < max_steps:
+        while not done['__all__'] and step < max_steps and not all(info['deadlock'].values()):
             # Chose an action for each agent in the environment
             for a in range(env.get_num_agents()):
                 if info['action_required'][a]:
